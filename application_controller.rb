1eb8cd4867db5e3db4f59fab8caad00e8e31362e
MilanoCookies <milanchopra10@gmail.com>
2015-07-02 14:50:43 +0000

    Joes Mom

--- application_controller.rb
+++ application_controller.rb
@@ -4,3 +4,13 @@ Bundler.require
 require './models/model.rb'
 
 class MyApp < Sinatra::Base
+  
+  get '/' do
+    erb :index
+  end
+  
+  post '/results' do
+    puts params
+    erb :results
+end
+end
