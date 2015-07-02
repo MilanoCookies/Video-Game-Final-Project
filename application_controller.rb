4320d355fa867730769c76fd1b35be437e13b455
trazzydazzy <19trazafindrakoto@unis.org>
2015-07-02 16:47:27 +0000

    I did ghetto spaghetti

--- application_controller.rb
+++ application_controller.rb
@@ -1,7 +1,7 @@
 require 'bundler'
 Bundler.require
 
-require './models/model.rb'
+require './models/Video_Games.rb'
 
 class MyApp < Sinatra::Base
 
@@ -9,11 +9,9 @@ class MyApp < Sinatra::Base
     erb :index
   end
 
-<<<<<<< HEAD
   post '/results' do
     puts params
     erb :results
 end
-=======
->>>>>>> 74d1e25ea928b02405ead8c39acf79c362219952
+
 end
